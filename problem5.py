de961face268476974bfd3ef7f777e578c4c67ca
jojofabe <jojofabe@vt.edu>
2014-08-28 17:43:23 -0400

    solved the problem

--- problem5.py
+++ problem5.py
@@ -1,13 +1,43 @@
+#only the dummy example
 primes = [2, 3, 5, 7, 11, 13, 17, 19] #listing out all of the primes from 1-20
-x = range(2, 20)
-factors = {}
+j = range(2, 20)
+a = {} #min
+b = {} #max
+for i in primes:
+	a[i] = 0
+	b[i] = 0
 
-for (i in x): 
-	for p in primes: 
-		if p < i:
-			if p % i == 0:  
-				smallFactor = p
-				i_temp = i/p
-				factors[i] = [p]
-				while(i_temp > p)
+for x in j:
+	if x in primes:
+		b[x] = b[x] + 1
+		if b[x] > a[x]:
+			a[x] = b[x]
+			b[x] = 0
+	else:
+		temp = x
+		i = 0
+		while i < len(primes) and temp != 1:
+			if temp % primes[i] == 0:
+				b[primes[i]] =  b[primes[i]] + 1
+				temp = temp/primes[i]
+			else:
+				i = i+1
+		print b
+		for i in b:
+			if a[i] < b[i]:
+				a[i] = b[i]
+			b[i] = 0
+N = 1
+for i in primes:
+	if a[i] < b[i]:
+		a[i] = b[i]
+		N = N * i**a[i]
+	else: 
+		N = N * i**a[i]
+
+print b
+print a
+print N
+
+
 
