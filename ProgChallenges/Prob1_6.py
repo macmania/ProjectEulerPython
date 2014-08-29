7f8d0eafa9ea893bf7428a5e69b8ad36e6d36c1f
jojofabe <jojofabe@vt.edu>
2014-08-29 19:21:57 -0400

    solved it :)

--- ProgChallenges/Prob1_6.py
+++ ProgChallenges/Prob1_6.py
@@ -1,13 +1,14 @@
+
 #Trick here is to decrement the 
 i = 1
 n = n_init = 1000
-end = 1
-listNums = range(end, n+1)
+end = 100
+listNums = range(201, 21+1)
 maxNum = 0
 
-for x in range(end, n+1):
+for x in range(900, 1000+1):
 	n = x
-	while(n > 900):
+	while(n > 1):
 		if n % 2 == 0: 
 			n = n >> 1 #same as n/2
 		else: 
