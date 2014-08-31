a6f31b32018192906cffa7ff45c7099276935718
jojofabe <jojofabe@vt.edu>
2014-08-30 20:56:39 -0400

    still need to finish this up

---
+++ ProgChallenges/Prob1_6_3.py
@@ -0,0 +1,22 @@
+#The trip UVA/PC problem
+
+f = input("test1.1_6")
+lines = iter(f.readLine())
+#start = next(lines)
+x = next(lines)
+for x != '0': 
+	if isinstance(x, int): 
+		y = [for z in range(x)]
+		i = 0
+		exp = next(lines)
+		sumExp = 0
+		while isinstance(exp, float): 
+			y[i] = float()
+			exp = next(lines)
+			sumExp += y[i]
+			i += 1
+		expDiv = sumExp/len(y)
+		posDif, negDif = 0, 0 
+
+		calculateMin()
+	
