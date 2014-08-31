2b213020a3211ea42ad214070ffb9a1dea7e246a
jojofabe <jojofabe@vt.edu>
2014-08-31 01:16:56 -0400

    adjusted the program to reflect the input test values for the PC/UVA question 'The Trip'

--- ProgChallenges/Prob1_6_3.py
+++ ProgChallenges/Prob1_6_3.py
@@ -5,35 +5,32 @@ lines = iter(f.read().splitlines())
 #start = next(lines)
 x = next(lines)
 
-while x != '0' or  x != 0:  
+while x != '0' and  x != 0:  
 	try: 
 		x = int(x)
 	except: 
 		break
-
 	if isinstance(x, int): 
 		y = [0 for z in range(x)]
 		i = 0
 		exp = float(next(lines))
 
 		sumExp = 0
-		while isinstance(exp, float) and exp != 0: 
-			print exp
+		while isinstance(exp, float) and exp != 0 and i < x: 
 			y[i] = float(exp)
 			exp = float(next(lines))
 			sumExp += y[i]
 			i += 1
-		print y
-		expDiv = sumExp/len(y)
+		expDiv = round(sumExp/len(y), 2)
 		posDif, negDif = 0, 0 
-
 		for j in y: 
 			temp = expDiv - j
 			if temp < 0:
 				negDif += temp
 			else:
 				posDif += temp
-		print negDif if ((negDif * -1) < posDif) else posDif
+		print '${0:.2f}'.format(negDif) if ((negDif * -1) < posDif) else '${0:.2f}'.format(posDif)
 		#print posDif
-	
+		y = []
 	x = exp
+	
